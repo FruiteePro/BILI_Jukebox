47d9dffcb5b0ff5c65765353d34acaa64d22cb6e
FruiteePro <messagejunpeng@outlook.com>
2022-11-18 19:43:00 +0800

    using thread instead of multiprocess

--- scheduler.py
+++ scheduler.py
@@ -3,33 +3,32 @@ import os
 import random
 import time
 from bilibili_api import live
+from threading import Thread, Lock
 
 from wordpicker import analysis_danmuku
 import utils
 import globaler as gl
 import ffmpeg_cmd
 
-room = live.LiveDanmaku(gl.room_id)
-
+room = live.LiveDanmaku(room_display_id=gl.room_id)
+lock = Lock()
 
 #主播放循环
 async def music_player():
     while True:
         try:
             code, selected_music = choose_music()
-            #gl.playing = selected_music
             _, duration = await ffmpeg_cmd.get_vidoe_info(selected_music)
             time_point1 = time.time()
             print("playing " + selected_music + " music_duration: " + str(duration) + " second")
             await begin_live(selected_music)
-            #await write_fifo(selected_music)
 
             if code:
                 utils.delete_music(selected_music)
             time_point2 = time.time()
             real_duration = time_point2 - time_point1
             print("music real play time: " + str(real_duration))
-            #await asyncio.sleep(10)
+            await asyncio.sleep(10)
         except Exception as e:
             print(e)
 
@@ -63,20 +62,30 @@ async def call_list(event):
     #     return
     # else:
     #     return
-    asyncio.gather(write_fifo())
-    
+    #asyncio.gather(write_fifo())
 
 
-
+#作废
 #video 写入 fifo
 async def write_fifo():
-    if gl.called_list:
-        video_name = gl.called_list[0]
-        fifo_path = './fifo/' + gl.pipe_name
-        print("23")
-        with open(fifo_path, 'w') as wf:
-            wf.write(video_name)
-            gl.called_list.pop(0)
+    try:
+        print("asking call_list")
+        if gl.called_list:
+            with lock:
+                lock.acquire()
+                video_name = gl.called_list[0]
+                gl.called_list.pop(0)
+                lock.release()
+            # fifo_path = './fifo/' + gl.pipe_name
+            # print("23")
+            # with open(fifo_path, 'w') as wf:
+            #     wf.write(video_name)
+            #     wf.flush()
+            #     gl.called_list.pop(0)
+            #     print("write_fifo success")
+    except Exception as e:
+        print(e)
+        print("write_fifo error")
         #with open(fifo_path, os.O_SYNC | os.O_CREAT | os.O_RDWR | os.O_NONBLOCK) as wf:
         # wf = os.open(fifo_path, os.O_SYNC | os.O_CREAT | os.O_RDWR)
         # os.write(wf, video_name.encode())
@@ -88,55 +97,65 @@ async def write_fifo():
 
 #添加一首 music
 async def add_music(music_name):
-    gl.called_list.append(music_name)
+    with lock:
+        gl.called_list.append(music_name)
 
 
 #选择下一个音乐
 #返回选择的 music_name
 def choose_music():
-    update_call_list()
+    #update_call_list()
     print(gl.called_list)
     if not gl.called_list:
         num = random.randint(0, len(gl.defult_list)-1)
         return 0, gl.defult_list[num]
     else:
-        res = gl.called_list[0];
-        gl.called_list.pop(0)
-        return 1, res
+        with lock:
+            res = gl.called_list[0];
+            gl.called_list.pop(0)
+            return 1, res
 
+#作废
 #更新call_list
 def update_call_list():
-    fifo_path = './fifo/' + gl.pipe_name
-    rf = os.open(fifo_path, os.O_RDONLY | os.O_NONBLOCK)
-    buffer = os.read(rf, 1024)
-    os.close(rf)
-    if len(buffer) == 0:
-        return -1
-    else:
-        muisc_names = utils.str_2_name(buffer)
-        gl.called_list.extend(muisc_names)
-        return 1
-            
+    lock.acquire()
+    if gl.called_list:
+        gl.called_list.extend(memlist)
+        memlist = []
+    lock.release()
+
+    # fifo_path = './fifo/' + gl.pipe_name
+    # rf = os.open(fifo_path, os.O_RDONLY | os.O_NONBLOCK)
+    # buffer = os.read(rf, 1024)
+    # os.close(rf)
+    # if len(buffer) == 0:
+    #     return -1
+    # else:
+    #     muisc_names = utils.str_2_name(buffer)
+    #     gl.called_list.extend(muisc_names)
+    #     return 1
 
 async def tasks():
-    #name_trans = write_fifo()
     room_connect = room.connect()
     await asyncio.gather(room_connect)
 
+def run_bili_cycle():
+    asyncio.run(tasks())
+    print("nice")
 
 
 def setup():
-    ret = os.fork()
-    if ret == 0:
-        asyncio.run(tasks())
-    else:
-        asyncio.run(music_player())
+    t1 = Thread(target=run_bili_cycle, args=())
+    t1.start()
+    asyncio.run(music_player())
+    t1.join()
+    # ret = os.fork()
+    # if ret == 0:
+    #     asyncio.run(tasks())
+    # else:
+    #     asyncio.run(music_player())
 
 
 if __name__ == "__main__":
-    name = "push"
-
-    #asyncio.run(setup())
-    utils.creat_FIFO(name)
     setup()
-    #ffmpeg_cmd.start_live
+
